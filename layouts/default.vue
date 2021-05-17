563078d5075e068af4331a6dcbb5a6bbb27cb341
Richard Pena <richardpena1102@gmail.com>
2021-05-16 23:18:41 -0400

    Adding a testing text in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,5 @@
 <template>
-  <v-app></v-app>
+  <div>This is just for testing</div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
-}
-</script>
+<script></script>
