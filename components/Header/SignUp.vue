bec5149ee422ffe5434b72cea49eb0c97efa3432
Richard Pena <richardpena1102@gmail.com>
2021-05-20 23:34:03 -0400

    adding sign in option

--- components/Header/SignUp.vue
+++ components/Header/SignUp.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div class="sign-up">
-      <div class="sign-up__title">Sign Up</div>
+      <div class="sign-up__title">
+        {{ isSigningUp ? 'Sign Up' : 'Sign in' }}
+      </div>
       <form class="sign-up__form" @submit.prevent="submit">
-        <div class="sign-up__form__name">
+        <div v-if="isSigningUp" class="sign-up__form__name">
           <label for="name">Name</label>
           <input v-model.trim="$v.name.$model" name="name" type="text" />
           <span v-if="$v.name.$invalid && $v.name.$dirty" class="error">
@@ -39,13 +41,22 @@
             {{ $v.password.$params.maxLength.max }} characters.
           </span>
         </div>
-        <div class="sign-up__form__switch">
-          <span class="sign-up__form__switch__text">
-            Already have an account? Then
-            <a class="sign-up__form__switch__link" href="#"> Log In </a>
-          </span>
+        <div class="sign-up__form__action">
+          <div class="sign-up__form__action__switch">
+            <span class="sign-up__form__action__switch__text">
+              {{ isSigningUp ? 'Already' : "Don't" }} have an account? Then
+              <span
+                class="sign-up__form__action__switch__link"
+                @click="changeSignUp"
+              >
+                {{ !isSigningUp ? 'Sign Up' : 'Sign in' }}
+              </span>
+            </span>
+          </div>
+          <button class="sign-up__form__action__submit" @click="submit">
+            Submit
+          </button>
         </div>
-        <button class="sign-up__form__submit" @click="submit">Submit</button>
       </form>
     </div>
   </div>
@@ -59,6 +70,7 @@ export default {
       password: '',
       name: '',
       correo: '',
+      isSigningUp: true,
     }
   },
   validations: {
@@ -76,8 +88,27 @@ export default {
     },
   },
   methods: {
+    changeSignUp() {
+      this.isSigningUp ? (this.isSigningUp = false) : (this.isSigningUp = true)
+    },
+    signIn() {
+      console.log('Sign In')
+    },
+    signUp() {
+      console.log('Sign Up')
+    },
     submit() {
       this.$v.$touch()
+      if (this.$v.$invalid) {
+        console.log('')
+      } else {
+        console.log('')
+        if (this.isSigningUp) {
+          this.signUp()
+        } else {
+          this.signIn()
+        }
+      }
     },
   },
 }
@@ -88,6 +119,7 @@ export default {
   background-color: white;
   border-radius: 10px;
   padding: 15px;
+  min-height: 400px;
   font-family: 'Roboto', sans-serif;
   width: 320px;
   position: relative;
@@ -100,9 +132,12 @@ export default {
     font-size: 24px;
   }
   &__form {
-    div {
+    &__name,
+    &__email,
+    &__password {
       display: flex;
       flex-direction: column;
+      position: relative;
       color: #001847;
       font-weight: 800;
       margin-top: 20px;
@@ -110,46 +145,59 @@ export default {
         margin-bottom: 10px;
       }
       input {
+        height: 30px;
+        padding: 0px 10px;
         border: #0f5ef7 1.5px solid;
         border-radius: 10px;
-        height: 30px;
         background-color: #f2f5fd;
         font-weight: 400;
         font-size: 14px;
       }
       .error {
+        position: absolute;
+        bottom: -20px;
+        right: 0;
         color: red;
         font-weight: 400;
         font-size: 12px;
         margin-top: 5px;
       }
     }
-    &__switch {
-      margin-bottom: 15px;
-      font-family: 'Roboto Condensed', sans-serif;
-      &__text {
-        font-size: 16px;
-        text-align: center;
-        font-weight: 400;
-      }
-      &__link {
-        font-weight: 700;
-        text-decoration: none;
-        color: #0f5ef7;
-        transition: color 500ms;
-        &:hover {
-          color: #001847;
+    &__action {
+      width: 100%;
+      padding: 15px;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      &__switch {
+        margin-bottom: 15px;
+        font-family: 'Roboto Condensed', sans-serif;
+        &__text {
+          display: block;
+          font-size: 16px;
+          font-weight: 400;
+          text-align: center;
+        }
+        &__link {
+          font-weight: 700;
+          text-decoration: none;
+          cursor: pointer;
+          color: #0f5ef7;
+          transition: color 500ms;
+          &:hover {
+            color: #001847;
+          }
         }
       }
-    }
-    &__submit {
-      background-color: #00bc8b;
-      font-family: 'Roboto', sans-serif;
-      color: white;
-      padding: 8px 0;
-      text-align: center;
-      width: 100%;
-      border-radius: 10px;
+      &__submit {
+        background-color: #00bc8b;
+        font-family: 'Roboto', sans-serif;
+        color: white;
+        padding: 8px 0;
+        text-align: center;
+        width: 100%;
+        border-radius: 10px;
+      }
     }
   }
 }
